fa179ad205d7784a407cc7475b9958c51476175d
swu_GURU2 <tina5004@naver.com>
2022-07-25 14:19:32 +0900

    change1

---
+++ app/src/main/java/com/example/guru2_team20/profile.kt
@@ -0,0 +1,11 @@
+package com.example.guru2_team20
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class profile : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profile)
+    }
+}
