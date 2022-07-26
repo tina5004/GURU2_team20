dbf1d2693c52e524b74242d3709cba7fb00383c8
pcr <qkrcofud926@gmail.com>
2022-07-26 09:23:11 +0900

    login & register

--- app/src/main/java/com/example/guru2_team20/LoginActivity.kt
+++ app/src/main/java/com/example/guru2_team20/LoginActivity.kt
@@ -1,34 +1,80 @@
 package com.example.guru2_team20
 
+import android.content.Intent
 import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class LoginActivity : AppCompatActivity() {
 
-    lateinit var EdtId : EditText
-    lateinit var EdtPwd : EditText
+    private var auth : FirebaseAuth? = null
+    lateinit var edtId : EditText
+    lateinit var edtPwd : EditText
     lateinit var btnLogin : Button
     lateinit var btnJoin : Button
-
-    lateinit var myDBHelper: SQLiteOpenHelper
-    lateinit var sqlDB : SQLiteDatabase
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
         setTitle("로그인");
 
-        EdtId = findViewById(R.id.EdtId)
-        EdtPwd = findViewById(R.id.EdtPwd)
-        btnJoin = findViewById(R.id.btnJoin)
+        edtId = findViewById(R.id.EdtId)
+        edtPwd = findViewById(R.id.EdtPwd)
         btnLogin = findViewById(R.id.btnLogin)
+        btnJoin = findViewById(R.id.btnJoin)
+
+        auth = Firebase.auth
+
+        btnJoin.setOnClickListener {
+            startActivity(Intent(this,RegisterActivity::class.java))
+        }
+
+        btnLogin.setOnClickListener {
+            signIn(edtId.text.toString(),edtPwd.text.toString())
+        }
 
 
+    }
+   /* public override fun onStart() {
+        super.onStart()
+        moveMainPage(auth?.currentUser)
+    }*/
 
+    // 로그인
+    private fun signIn(email: String, password: String) {
 
+        if (email.isNotEmpty() && password.isNotEmpty()) {
+            auth?.signInWithEmailAndPassword(email, password)
+                ?.addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        Toast.makeText(
+                            baseContext, "로그인에 성공 하였습니다.",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                        moveMainPage(auth?.currentUser)
+                    } else {
+                        Toast.makeText(
+                            baseContext, "로그인에 실패 하였습니다.",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
+                }
+        }
+    }
+
+    // 유저정보 넘겨주고 메인 액티비티 호출
+    fun moveMainPage(user: FirebaseUser?){
+        if( user!= null){
+            startActivity(Intent(this,MainActivity::class.java))
+            finish()
+        }
     }
 }
--- app/src/main/java/com/example/guru2_team20/RegisterActivity.kt
+++ app/src/main/java/com/example/guru2_team20/RegisterActivity.kt
@@ -2,10 +2,52 @@ package com.example.guru2_team20
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class RegisterActivity : AppCompatActivity() {
+    lateinit var btnRegister : Button
+    lateinit var edtNewId : EditText
+    lateinit var edtNewPassword : EditText
+
+    private var auth : FirebaseAuth? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+
+        btnRegister = findViewById(R.id.btnRegister)
+        edtNewId = findViewById(R.id.edtNewId)
+        edtNewPassword = findViewById(R.id.edtNewPassword)
+
+        auth = Firebase.auth
+
+        btnRegister.setOnClickListener {
+            createAccount(edtNewId.text.toString(),edtNewPassword.text.toString()) }
+    }
+
+    private fun createAccount(email: String, password: String) {
+
+        if (email.isNotEmpty() && password.isNotEmpty()) {
+            auth?.createUserWithEmailAndPassword(email, password)
+                ?.addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        Toast.makeText(
+                            this, "계정 생성 완료.",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                        finish() // 가입창 종료
+                    } else {
+                        Toast.makeText(
+                            this, "계정 생성 실패",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
+                }
+        }
     }
 }
