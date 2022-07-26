d3aa0d00caaea493e09bb9092f03ed82c6de2019
ommmdong <dhaehd07@naver.com>
2022-07-26 12:20:21 +0900

    지도띄우기

--- app/src/main/java/com/example/guru2_team20/MapsActivity.kt
+++ app/src/main/java/com/example/guru2_team20/MapsActivity.kt
@@ -1,5 +1,8 @@
 package com.example.guru2_team20
 
+import android.graphics.Bitmap
+import android.graphics.drawable.BitmapDrawable
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
@@ -7,10 +10,8 @@ import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
-import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.model.MarkerOptions
 import com.example.guru2_team20.databinding.ActivityMapsBinding
-import com.google.android.gms.maps.model.CameraPosition
+import com.google.android.gms.maps.model.*
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
@@ -33,22 +34,44 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
 
-        val seoul = LatLng(37.6281, 127.0905)
+        // Add a marker in Sydney and move the camera
+        val seoul = LatLng(37.566,126.9779)
+
+        //마커 아이콘 만들기
+        val descriptor = getDescriptorFromDrawable(R.drawable.marker)
 
         //마커
         val marker = MarkerOptions()
             .position(seoul)
-            .title("Marker in Seoul Women's Univ")
+            .title("Marker is Seoul")
+            .icon(descriptor)
+
         mMap.addMarker(marker)
 
-        //카메라 위치
+        //카메라의 위치
         val cameraOption = CameraPosition.Builder()
             .target(seoul)
-            .zoom(17f)
+            .zoom(15f)
             .build()
 
-        val camera = CameraUpdateFactory.newCameraPosition(cameraOption)
+        val camera = CameraUpdateFactory.newCameraPosition((cameraOption))
 
         mMap.moveCamera(camera)
+
+
+    }
+
+    fun getDescriptorFromDrawable(drawableID : Int) : BitmapDescriptor {
+        var bitmapDrawable: BitmapDrawable
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            bitmapDrawable = getDrawable(drawableID) as BitmapDrawable
+        }
+        else{
+            bitmapDrawable = resources.getDrawable(drawableID)as BitmapDrawable
+        }
+
+        //마커 크기변환
+        val scaledBitmap= Bitmap.createScaledBitmap(bitmapDrawable.bitmap, 100,100,false)
+        return BitmapDescriptorFactory.fromBitmap(scaledBitmap)
     }
 }
